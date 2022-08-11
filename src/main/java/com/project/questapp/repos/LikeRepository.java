80dd5d7d8b124c3a1b938cb989bdba2f961cd7e0
Feyyaz Begen <begenfeyyaz@gmail.com>
2022-08-11 17:03:53 +0300

    Like ve Comment Controller,Repo,Service Eklendi

--- src/main/java/com/project/questapp/repos/LikeRepository.java
+++ src/main/java/com/project/questapp/repos/LikeRepository.java
@@ -3,5 +3,12 @@ package com.project.questapp.repos;
 import com.project.questapp.entities.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikeRepository extends JpaRepository<Like,Long> {
+    List<Like> findByPostId(Long aLong);
+
+    List<Like> findByUserId(Long aLong);
+
+    List<Like> findByUserIdAndPostId(Long aLong, Long aLong1);
 }
